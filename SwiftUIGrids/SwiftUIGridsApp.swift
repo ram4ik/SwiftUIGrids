bbf500560a9d839932383e236488c1aea04044d8
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 08:34:41 +0300

    Initial Commit

---
+++ SwiftUIGrids/SwiftUIGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGridsApp.swift
+//  SwiftUIGrids
+//
+//  Created by Ramill Ibragimov on 23.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
